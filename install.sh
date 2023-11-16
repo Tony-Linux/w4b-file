44b2399965b3f560a1374d4d70808097dd6ce91f
Mr Fidal <140599524+mrfidal@users.noreply.github.com>
2023-11-16 11:54:21 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+############################################################################
+#                      © 2023 Tony-Linux. All Rights Reserved.             #
+#          For inquiries regarding this project, kindly reach out to:      #
+#                    Email: mrhunter5@proton.me                            #
+#                                                                          #
+#    This content is protected by copyright law and may not be replicated  #
+#     or altered without the explicit written consent of the copyright     #
+#                               holder.                                    #
+############################################################################
+
 if [[ -d /usr/bin ]]; then
 	if [ $(id -u) -ne 0 ]; then
 		echo -e "${white} [${red}+${white}] ${red}This script necessitates root access to execute."
